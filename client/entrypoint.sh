98cb9c3e01e52ebd78a560ce73116b90d7b23058
Julian Brunner <julianbrunner@gmail.com>
2021-09-27 01:57:55 +0200

    updated notes

--- client/entrypoint.sh
+++ client/entrypoint.sh
@@ -3,11 +3,12 @@
 # prevent expansion of '*' in crontab frequency
 set -f
 
+# TODO: look at compression options
 BORG_COMMAND="borg create --list --filter AME --stats ::$BORG_PREFIX-{now} /data"
 
-echo "$BORG_FREQUENCY $BORG_COMMAND > /proc/1/fd/1 2> /proc/1/fd/2" > entries.txt
-
-crontab entries.txt
+echo "$BORG_FREQUENCY $BORG_COMMAND > /proc/1/fd/1 2> /proc/1/fd/2" > /root/entries.txt
+crontab /root/entries.txt
+rm /root/entries.txt
 
 # TODO: remove entries, maybe also use tmp file
 
