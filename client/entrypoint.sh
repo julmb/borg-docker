f77c50fba8f7e4a98e4b8997c0d15b64f1be4bdb
Julian Brunner <julianbrunner@gmail.com>
2021-09-27 18:49:53 +0200

    supply crontab directly

--- client/entrypoint.sh
+++ client/entrypoint.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-# prevent expansion of '*' in crontab frequency
-set -f
+crontab /root/crontab
 
-BORG_COMMAND="borg create --verbose --list --filter AME --stats --compression $BORG_COMPRESSION ::$BORG_PREFIX-{now} $BORG_SOURCE"
-
-echo "$BORG_FREQUENCY $BORG_COMMAND > /proc/1/fd/1 2> /proc/1/fd/2" | crontab -
-
-exec $@
+exec "$@"
