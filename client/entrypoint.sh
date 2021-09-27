cb2589f10ae3c76b4186051291ab1f3dfa1c13df
Julian Brunner <julianbrunner@gmail.com>
2021-09-27 02:01:34 +0200

    cleanup

--- client/entrypoint.sh
+++ client/entrypoint.sh
@@ -6,10 +6,6 @@ set -f
 # TODO: look at compression options
 BORG_COMMAND="borg create --list --filter AME --stats ::$BORG_PREFIX-{now} /data"
 
-echo "$BORG_FREQUENCY $BORG_COMMAND > /proc/1/fd/1 2> /proc/1/fd/2" > /root/entries.txt
-crontab /root/entries.txt
-rm /root/entries.txt
-
-# TODO: remove entries, maybe also use tmp file
+echo "$BORG_FREQUENCY $BORG_COMMAND > /proc/1/fd/1 2> /proc/1/fd/2" | crontab -
 
 exec $@
