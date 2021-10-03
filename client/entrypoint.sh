c635dd44b1a6e40e37b7032f1d13d428e1fb1513
Julian Brunner <julianbrunner@gmail.com>
2021-10-03 14:58:52 +0200

    new client entrypoint

--- client/entrypoint.sh
+++ client/entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
-crontab /root/crontab
+echo "rebuilding known hosts..."
+mkdir --parents ~/.ssh
+read type key host < ~/ssh_host_ed25519_key.pub
+echo $host $type $key > ~/.ssh/known_hosts
+
+echo "rebuilding crontab..."
+BORG_REPO=$repository
+BORG_PASSPHRASE=$passphrase
+echo "$time $command ::{now} $source > /proc/1/fd/1 2> /proc/1/fd/2" | crontab -
 
 exec "$@"
