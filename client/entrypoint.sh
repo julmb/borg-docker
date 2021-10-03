b1d7ec992b8a75e4e95b28ecdaa374208d82e3bc
Julian Brunner <julianbrunner@gmail.com>
2021-10-04 00:35:24 +0200

    cleanup entrypoint

--- client/entrypoint.sh
+++ client/entrypoint.sh
@@ -6,8 +6,9 @@ read type key host < ~/ssh_host_ed25519_key.pub
 echo $host $type $key > ~/.ssh/known_hosts
 
 echo "rebuilding crontab..."
+echo "$time $command > /proc/1/fd/1 2> /proc/1/fd/2" | crontab -
+
 export BORG_REPO=$repository
 export BORG_PASSPHRASE=$passphrase
-echo "$time $command > /proc/1/fd/1 2> /proc/1/fd/2" | crontab -
 
 exec "$@"
