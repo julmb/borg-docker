33f314f0ed36011392a4c6e026f408d52622e3d9
Julian Brunner <julianbrunner@gmail.com>
2021-10-03 15:06:32 +0200

    export borg variables

--- client/entrypoint.sh
+++ client/entrypoint.sh
@@ -6,8 +6,8 @@ read type key host < ~/ssh_host_ed25519_key.pub
 echo $host $type $key > ~/.ssh/known_hosts
 
 echo "rebuilding crontab..."
-BORG_REPO=$repository
-BORG_PASSPHRASE=$passphrase
+export BORG_REPO=$repository
+export BORG_PASSPHRASE=$passphrase
 echo "$time $command ::{now} $source > /proc/1/fd/1 2> /proc/1/fd/2" | crontab -
 
 exec "$@"
