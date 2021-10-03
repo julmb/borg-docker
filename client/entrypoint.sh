bd3bda4fa97c7f637c877ae4d512f7cc4ab7638e
Julian Brunner <julianbrunner@gmail.com>
2021-10-03 22:24:23 +0200

    unified commands

--- client/entrypoint.sh
+++ client/entrypoint.sh
@@ -8,6 +8,6 @@ echo $host $type $key > ~/.ssh/known_hosts
 echo "rebuilding crontab..."
 export BORG_REPO=$repository
 export BORG_PASSPHRASE=$passphrase
-echo "$time $command ::{now} $source > /proc/1/fd/1 2> /proc/1/fd/2" | crontab -
+echo "$time $command > /proc/1/fd/1 2> /proc/1/fd/2" | crontab -
 
 exec "$@"
